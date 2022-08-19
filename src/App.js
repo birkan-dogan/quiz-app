a216e90fa91cea5a9c7d20c26ccecbdbe55c60f2
birkan <b.561birkan@gmail.com>
2022-08-19 14:53:34 +0300

    displaying data on the screen

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Loading from "./components/Loading";
 import SetupForm from "./components/SetupForm";
 import { useGlobalContext } from "./context/QuizContext";
+import Modal from "./components/Modal";
 
 function App() {
   const { waiting, loading, questions, index, correct } = useGlobalContext(); // consuming the context
@@ -12,7 +13,34 @@ function App() {
   if (loading) {
     return <Loading />;
   }
-  return <main>quiz app</main>;
+  // console.log(questions);
+  const { question, incorrect_answers, correct_answer } = questions[index];
+  const answers = [...incorrect_answers, correct_answer];
+  return (
+    <main>
+      <Modal />
+      <section className="quiz">
+        <p className="correct-answers">
+          correct answers : {correct}/{index}
+        </p>
+        <article className="container">
+          <h2 dangerouslySetInnerHTML={{ __html: question }} />
+          <div className="btn-container">
+            {answers.map((answer, index) => {
+              return (
+                <button
+                  key={index}
+                  className="answer-btn"
+                  dangerouslySetInnerHTML={{ __html: answer }}
+                />
+              );
+            })}
+          </div>
+        </article>
+        <button className="next-question">next question</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
