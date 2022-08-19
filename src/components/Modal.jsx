432052c9f7c3b9fcf8c891e0ab90502585668af0
birkan <b.561birkan@gmail.com>
2022-08-19 10:18:46 +0300

    creating new project and building structure

---
+++ src/components/Modal.jsx
@@ -0,0 +1,8 @@
+const Modal = () => {
+  return (
+    <div>
+      <h1>modal</h1>
+    </div>
+  );
+};
+export default Modal;
---
+++ src/components/SetupForm.jsx
@@ -0,0 +1,8 @@
+const SetupForm = () => {
+  return (
+    <div>
+      <h1>setupform</h1>
+    </div>
+  );
+};
+export default SetupForm;
